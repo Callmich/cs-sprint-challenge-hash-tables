2bb583878d57f84d193d6dae73db8e65d4be8a94
Michael Callahan <CallahanMichaelE@gmail.com>
2020-08-07 11:13:46 -0600

    some psedudo code for number 5 and a start to try to create a dictionary

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,56 @@
+# # File Finder
+# Given a list of full paths to files, and a list of filenames to query,
+# report all the full paths that match that filename.
+# Example input:
+
+# paths = [
+#     "/usr/local/share/foo.txt",
+#     "/usr/bin/ls",
+#     "/home/davidlightman/foo.txt",
+#     "/bin/su"
+# ]
+# queries = [
+#     "ls",
+#     "foo.txt",
+#     "nosuchfile.txt"
+# ]
+
+
+# Example return value:
+
+# ```
+# [ "/usr/local/share/foo.txt", "/usr/bin/ls", "/home/davidlightman/foo.txt" ]
+# ```
+
+# because that's where the `foo.txt` and `ls` files are. 
+
+# The file `"nosuchfile.txt"` is ignored because it's not in the `paths`.
+
+# Return list can be in any order.
+
+# Limits:
+
+# * Up to approximately 1,000,000 paths.
+# * Up to approximately 1,000,000 queries.
+
+
+
 # Your code here
 
 
 
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # split each file name at the /
+    # use the last element as the ket in a dict
+    # loop through the dict finding if the request is in the dict
+        # if so append onto the answers
+
+    my_dict = {}
+    result = []
+
+    for fle in files:
+        fle.split('/')
+        my_dict[fle[-1]] = fle
 
     return result
 
